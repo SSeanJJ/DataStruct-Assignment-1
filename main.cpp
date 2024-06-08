83b0da45eecd022feca35e8d3596a2c0526cd7fe
StevenRotelli <95944145+StevenRotelli@users.noreply.github.com>
2024-06-08 17:02:39 -0500

    added main methods

--- main.cpp
+++ main.cpp
@@ -38,23 +38,54 @@ int main(){
                input_rec.last_name = rtrim(input_rec.last_name);
                input_rec.first_name = rtrim(input_rec.first_name);
                input_rec.code = string_to_int(code_string);
+                            
+                bool insert = true;
+                for(int i = 0; i < record_list.size(); i++){
+                    Personal_record current;
+                    record_list.retrieve(i, current);
+                    if(current.last_name == input_rec.last_name && current.first_name == input_rec.first_name){
+                        insert = false;
+                        break; // Duplicate found, do not insert
+                    }
+                }
 
-               // write code to implement Requirement 1
- 
+                if(insert){
+                    int i = 0;
+                    Personal_record current;
+                    while(i < record_list.size() && 
+                          (record_list.retrieve(i, current), 
+                          current.last_name < input_rec.last_name ||
+                          (current.last_name == input_rec.last_name && current.first_name < input_rec.first_name))){
+                        i++;
+                    }
+                    record_list.insert(i, input_rec);
+                }
             }
          } else
             cout << "Invalid file name." << endl;
       }
       else if(user_input == "2"){
+         if (record_list.size() == 0)
+         { 
+            cout << "List empty";
+            continue;
+         }
+         for(int i = 0; i < record_list.size(); i++)
+         {
+            Personal_record pr;
+            record_list.retrieve(i, pr);
 
-      
-
-      // write code to implement Requirement 2
-
-      
-      
+            cout << "Last Name:" << pr.last_name << endl;
+            cout << "First Name:" << pr.first_name << endl;
+            cout << "Code:" << pr.code << endl;
+         }
       }
       else if(user_input == "3"){
+         if (record_list.size() == 0)
+         { 
+            cout << "List empty";
+            continue;
+         }
          string search_first_name, search_last_name;
          cout << endl;
          cout << "Enter the last name to search:" << endl;
@@ -64,13 +95,21 @@ int main(){
          getline(cin, search_first_name);
          start = clock();
 
-
-
-         // write code to implement Requirement 3
-
-
-
-
+         bool found = false;
+         for (int i = 0; i < record_list.size(); i++) {
+             Personal_record pr;
+             record_list.retrieve(i, pr);
+             if (pr.last_name == search_last_name && pr.first_name == search_first_name) {
+                 cout << "Last Name: " << pr.last_name << endl;
+                 cout << "First Name: " << pr.first_name << endl;
+                 cout << "Code: " << pr.code << endl;
+                 found = true;
+                 break;
+             }
+         }
+         if (!found) {
+             cout << "Not found." << endl;
+         }
 
          finish = clock();
          elapsed_time = (double)(finish - start) / CLOCKS_PER_SEC;
